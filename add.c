9ed8ac92e699cae8812c1ea63af633244f01eb38
sumeet-vayavyalabs <sumeets@vayavyalabs.com>
2025-01-16 21:27:25 +0530

    added add.c file

---
+++ add.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int add(int a,int b){
+	return a+b;
+}
+
+int main(){
+	int a;
+	scanf("%d",&a);
+	int b;
+	scanf("%d",&b);
+	int c = add(a,b);
+	printf("addition of %d and %d is %d\n",a,b,c);
+}
